2edaf09f64902499a61a9a3fc25af8d6f89e3e10
Jinping4038 <jinping4038@gmail.com>
2023-04-23 19:42:46 -0700

    Initial Commit

---
+++ DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by king on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
